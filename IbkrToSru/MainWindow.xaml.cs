63f68c2a48308c43d07add1691b47f3d81b6a86e
Johan Larsson <johan_gavle1@hotmail.com>
2022-04-26 17:33:27 +0200

    Save button

--- IbkrToSru/MainWindow.xaml.cs
+++ IbkrToSru/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace IbkrToSru;
 
+using System.IO;
 using System.Windows;
 using Microsoft.Win32;
 
@@ -20,6 +21,20 @@ public partial class MainWindow : Window
         if (dialog.ShowDialog() == true)
         {
             ((MainViewModel)this.DataContext).CsvFile = dialog.FileName;
+        }
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new SaveFileDialog()
+        {
+            DefaultExt = ".sru",
+            Filter = "SRU files|*.sru",
+        };
+
+        if (dialog.ShowDialog() == true)
+        {
+            File.WriteAllText(dialog.FileName, ((MainViewModel)this.DataContext).SruText);
         }
     }
 }
